e5f2ab5d35ea2519d3b65d7c9dac81629d2ccaab
belinskyc <36079242+belinskyc@users.noreply.github.com>
2023-12-14 16:09:23 -0500

    hgv

--- test.R
+++ test.R
@@ -1,3 +1,4 @@
 a = read.csv("https://belinskyc.github.io/shinyliveTest/a.csv");
 #b = readRDS("docs/QFC_ClassInfo.rds");
 b = readRDS(url("https://belinskyc.github.io/shinyliveTest/QFC_ClassInfo.rds"));
+v 
